5cd99cbfd9993c263235dc558ebb6e14e7594c0b
FangWolf <siyu2510@163.com>
2018-04-13 14:39:43 +0800

    添加自动刷新

--- app/src/main/java/com/fangwolf/coldweather/WeatherActivity.java
+++ app/src/main/java/com/fangwolf/coldweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.fangwolf.coldweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.fangwolf.coldweather.gson.Forecast;
 import com.fangwolf.coldweather.gson.Weather;
+import com.fangwolf.coldweather.service.AutoUpdateService;
 import com.fangwolf.coldweather.util.HttpUtil;
 import com.fangwolf.coldweather.util.Utility;
 
@@ -216,5 +218,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
